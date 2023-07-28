84812f20261991221d6bd01b8a172987b24cc9a0
Ziadsakr09 <ziadsakr01@gmail.com>
2023-07-28 21:07:48 +0300

    assignment 2 added

---
+++ Unit 4( Data structures )/Lesson2/Collision Avoidance/DC.h
@@ -0,0 +1,34 @@
+/*
+ * DC.h
+ *
+ *  Created on: Jul 28, 2023
+ *      Author: Ahmed
+ */
+
+
+
+
+#ifndef DC_H_
+#define DC_H_
+
+#include "state.h"
+
+//DC Motor states
+enum
+{
+	DC_Motor_idle,
+	DC_Motor_busy
+}DC_Motor_State_Id;
+
+
+STATE_Define(DC_Motor_idle);
+STATE_Define(DC_Motor_busy);
+
+
+extern void (*DC_Motor_State)();
+
+
+void DC_Motor_init();
+
+
+#endif /* DC_H_ */
---
+++ Unit 4( Data structures )/Lesson2/Collision Avoidance/US.h
@@ -0,0 +1,30 @@
+/*
+ * US.h
+ *
+ *  Created on: Jul 28, 2023
+ *      Author: Ahmed
+ */
+
+
+
+
+#ifndef US_H_
+#define US_H_
+#include "state.h"
+enum
+{
+	US_busy
+}US_Sensor_State_Id;
+
+
+STATE_Define(US_Sensor_busy);
+
+
+extern void (*US_Sensor_State)();
+
+
+void US_Sensor_init();
+int US_Get_Distance_Random(int min, int max, int count);
+
+
+#endif /* US_H_ */
